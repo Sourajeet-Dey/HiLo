67c592bc00f7003689945f904b58f79e385591a6
sourajeet_dey <write2sjdey@gmail.com>
2023-08-25 12:41:57 +0530

    Created a basic structure for game

--- src/example/HiLo.java
+++ src/example/HiLo.java
@@ -1,13 +1,22 @@
 package example;
+import java.util.Scanner;
 
 public class HiLo {
-
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		// Create a random number for the user to guess
-		for(int i=0; i<20; i++) {
-			int theNumber = (int) (Math.random()*100+1);
-			System.out.println(theNumber);
-		}
+		int theNumber = (int) (Math.random()*100+1);
+//		System.out.println(theNumber);
+		int guess = 0;
+		while(guess != theNumber) {
+			System.out.println("Guess a number between 1 and 100:");
+			guess = scan.nextInt();
+			if(guess < theNumber)
+				System.out.println(guess + " is too low. Try again.");
+			else if(guess < theNumber)
+				System.out.println(guess + " is too high. Try again.");
+			else
+				System.out.println(guess + " is correct. You win!");
+		}	// End of while loop for guessing
 	}
-
 }
