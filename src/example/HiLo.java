54f16031db19f0c649a615f0fb36b99019f3e37c
sourajeet_dey <write2sjdey@gmail.com>
2023-08-25 14:45:47 +0530

    Added more functionalities

--- src/example/HiLo.java
+++ src/example/HiLo.java
@@ -4,19 +4,41 @@ import java.util.Scanner;
 public class HiLo {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		// Create a random number for the user to guess
-		int theNumber = (int) (Math.random()*100+1);
-//		System.out.println(theNumber);
-		int guess = 0;
-		while(guess != theNumber) {
-			System.out.println("Guess a number between 1 and 100:");
-			guess = scan.nextInt();
-			if(guess < theNumber)
-				System.out.println(guess + " is too low. Try again.");
-			else if(guess < theNumber)
-				System.out.println(guess + " is too high. Try again.");
-			else
-				System.out.println(guess + " is correct. You win!");
-		}	// End of while loop for guessing
+		String playAgain = "";
+		do {
+			// Create a random number for the user to guess
+			int theNumber = (int) (Math.random()*1000+1);
+			System.out.println(theNumber);
+			int guess = 0;
+			int guessCount = 0;
+			while(guess != theNumber) {
+				System.out.println("Guess a number between 1 and 100:");
+				guess = scan.nextInt();
+				guessCount++;
+				if(guess < theNumber) {
+					if(theNumber - guess > 25)
+						System.out.println(guess + " is too low. Try again.");
+					else
+						System.out.println(guess + " is slightly low. Try again.");
+				}
+				else if(guess > theNumber) {
+					if(guess - theNumber > 25)
+						System.out.println(guess + " is too high. Try again.");
+					else
+						System.out.println(guess + " is slightly high. Try again.");
+				}
+				else {
+					System.out.println(guess + " is correct. You win!");
+					if(guessCount <= 10)
+						System.out.println("It took you only " + guessCount + " tries. Good job!");
+					else
+						System.out.println("It took you " + guessCount + " tries. Can you do better?");
+				}
+			}	// End of while loop for guessing	
+			System.out.println("Would you like to play again (y/n)?");
+			playAgain = scan.next();
+		}while(playAgain.equalsIgnoreCase("y"));
+		System.out.print("Thanks for playing, Goodbye!");
+		scan.close();
 	}
 }
